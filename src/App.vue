015b3a6255c4106ae86a9667a64f4deab3db6908
Romain Durand Saint Omer <romain.dso@gmail.com>
2017-09-30 12:45:02 +0200

    init app

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <blockquote>
+      <h1>{{ story.text }}</h1>
+      <span>- {{ story.author }}</span>
+    </blockquote>
+  </div>
+</template>
+
+<script>
+import data from './data'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      story: data.stories[Math.floor(Math.random() * data.stories.length)]  
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+html, body {
+  color: #42b983;
+  background-color: #2c3e50;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+
+blockquote {
+  width: 950px;
+  margin: 0 auto;
+
+  h1 {
+    font-size: 40px;
+    font-style: italic;
+    padding-left: 15px;
+    border-left: 1px solid #42b983;
+    text-align: left;
+
+    &::before, &::after {
+      content: '"';
+    }
+  }
+  
+  span {
+    display: block;
+    color: #ccc;
+    font-size: 30px;
+    text-align: left;
+  }
+}
+</style>
---
+++ src/data.js
@@ -0,0 +1,14 @@
+export default {
+  stories: [{
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et luctus leo.',
+    author: 'Anonymous'
+  },
+  {
+    text: 'Maecenas interdum ultricies egestas. Curabitur fringilla ac orci ac interdum. Donec sodales ante id efficitur molestie.',
+    author: 'Anonymous'
+  },
+  {
+    text: 'Nullam eu posuere neque, a porta velit.',
+    author: 'Anonymous'
+  }]
+}
