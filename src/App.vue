cfa1fe82ab86972f8dd74ec3e90ce4b2880de9f4
Romain Durand Saint Omer <romain.dso@gmail.com>
2017-09-30 15:48:08 +0200

    clean css

--- src/App.vue
+++ src/App.vue
@@ -31,29 +31,4 @@ html, body {
   text-align: center;
   margin-top: 60px;
 }
-
-blockquote {
-  width: 800px;
-  margin: 0 auto;
-
-  h1 {
-    font-family: 'Lobster', cursive;
-    font-size: 40px;
-    font-weight: 400;
-    padding-left: 15px;
-    border-left: 1px solid #42b983;
-    text-align: left;
-
-    &::before, &::after {
-      content: '" ';
-    }
-  }
-  
-  span {
-    display: block;
-    color: #ccc;
-    font-size: 30px;
-    text-align: left;
-  }
-}
 </style>
