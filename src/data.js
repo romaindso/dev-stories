82243da480ae087eb76ecd33d6dbe09c7d9497eb
Romain Durand Saint Omer <romain.dso@gmail.com>
2018-06-29 20:01:44 +0200

    feat: add real data

--- src/data.js
+++ src/data.js
@@ -1,14 +1,40 @@
 export default {
   stories: [{
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et luctus leo.',
-    author: 'Anonymous'
+    text: `En fait c'est pas compliqué, faut juste comprendre le code`,
+    author: 'Un dev'
   },
   {
-    text: 'Maecenas interdum ultricies egestas. Curabitur hringilla ac orci ac interdum. Donec sodales ante id efficitur molestie.',
-    author: 'Anonymous'
+    text: `Mais je comprends pas, c'est pas compliqué un arbre`,
+    author: 'Le PO'
   },
   {
-    text: 'Nullam eu posuere neque, a porta velit.',
-    author: 'Anonymous'
+    text: `Loic fait dans le slip`,
+    author: 'Le PO dans le fashion'
+  },
+  {
+    text: `Plus c'est long moins y'a de body`,
+    author: 'Un dev'
+  },
+  {
+    text: `Gitlab c'est peut être fait en Vue.js mais c'est bien lent`,
+    author: 'Un dev'
+  },
+  {
+    text: `Le problème c'est pas que tu m'écoutes pas, c'est que tu me comprends pas`,
+    author: 'Un couple de dev'
+  },
+  {
+    text: `La pente est rude mais la route est droite`,
+    author: 'Un dev sur un nouveau projet'
+  }, {
+    text: `Qui dit non consent`,
+    author: 'Un dev à propos de sa MR'
+  }, {
+    text: `Le PHP c'est comme Johny, c'est mort`,
+    author: 'Un dev JS'
+  },
+  {
+    text: `Il est où ton site ? Je l'ai pas VUE`,
+    author: 'Un dev JS'
   }]
 }
