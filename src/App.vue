2d4115a5b0b7b30a0148941835846c554809d0f1
Romain Durand Saint Omer <romain.dso@gmail.com>
2018-07-01 20:20:41 +0200

    feat: add next button and improve ux

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="app">
-    <div>
-      <h1>Dev stories</h1>
-      <story></story>
-    </div>
+    <header>
+        <h1>Dev stories</h1>
+      </header>
+    <story></story>
     <footer>
       <a href="https://github.com/romaindso/dev-stories/pulls" target="_blank">Soumettre une story</a>
+      <a href="https://github.com/romaindso/dev-stories/" class="button button-next">Next story</a>
     </footer>
   </div>
 </template>
@@ -41,19 +42,56 @@ body {
   transform: translate(-50%, -50%);
 }
 
-#app > div > h1 {
-  font-family: "Lobster", cursive;
-  color: white;
-  font-size: 100px;
+header {
+  position: relative;
+
+  & > h1 {
+    font-family: "Lobster", cursive;
+    color: white;
+    font-size: 100px;
+    margin: 10px 0;
+  }
 }
 
 footer {
   position: sticky;
-  top: 0;
+  text-align: center;
+  margin-top: 40px;
+
+  a {
+    color: white;
+    font-style: italic;
+  }
 }
 
-footer a {
-  color: white;
-  font-style: italic;
+.button-next {
+  margin-left: 20px;
+}
+
+a.button {
+  display: inline-block;
+  padding: 0.7em 1.4em;
+  border-radius: 0.15em;
+  box-sizing: border-box;
+  text-decoration: none;
+  font-family: "Avenir", sans-serif;
+  text-transform: uppercase;
+  font-weight: 400;
+  color: #ffffff;
+  background-color: #42b983;
+  box-shadow: inset 0 -0.6em 0 -0.35em rgba(0, 0, 0, 0.17);
+  text-align: center;
+  position: relative;
+}
+
+a.button:active {
+  top: 0.1em;
+}
+
+@media all and (max-width: 30em) {
+   a.button {
+    display: block;
+    margin: 0.4em auto;
+  }
 }
 </style>
